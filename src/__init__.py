866a35d430f9c6114e67f04ac9f1e63bdca0c05e
jeffmaine <oyeniyij43@gmail.com>
2025-02-03 06:45:28 +0100

    Added CORS

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.route import number_router
 
 version = "v1"
@@ -7,6 +8,14 @@ app = FastAPI(
     title="Number Classification API",
     description="An API that takes a number and returns interesting mathematical properties about it, along with a fun fact",
     version=version
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
 )
 
 
